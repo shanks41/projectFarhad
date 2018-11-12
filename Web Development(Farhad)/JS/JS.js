c87d51294de3332251fe1145ba15f971031795a6
Shanker.Bhattarai <Shanker.bhattarai@edu.turkuamk.fi>
2018-11-12 23:37:53 +0200

    Project

---
+++ Web Development(Farhad)/JS/JS.js
@@ -0,0 +1,31 @@
+function showtime(){
+    var date= new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = "AM";
+    
+    if (h == 0){
+        h == 12;
+    }
+    if (h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("footer").innerText = time;
+}
+
+function mouseOver(){
+    window.alert("Your mouse was on first project");
+    }
+function scrollFunction(){
+     window.alert("Page got scrolled up and down!");
+    }
+function loadFunction(){
+    alert("loaded");
+}
